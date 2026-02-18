72b0b9a47e512f06f099028bd5ee9c0ba6be681f
Athanasios Douitsis <65439+aduitsis@users.noreply.github.com>
2026-02-18 02:24:26 +0200

    Add git_timeout option to defaults
    Added git_timeout option to configure command timeout.

--- lua/gitlinker/opts.lua
+++ lua/gitlinker/opts.lua
@@ -1,10 +1,13 @@
 local M = {}
 
+local git = require("gitlinker.git")
+
 local defaults = {
   remote = "origin", -- force the use of a specific remote
   add_current_line_on_normal_mode = true, -- if true adds the line nr in the url for normal mode
   action_callback = require("gitlinker.actions").copy_to_clipboard, -- callback for what to do with the url
   print_url = true, -- print the url after action
+  git_timeout = 5000, -- by default, any git command after 5 seconds.
 }
 
 local opts
@@ -14,6 +17,9 @@ function M.setup(user_opts)
     opts = vim.tbl_deep_extend("force", {}, defaults)
   end
   opts = vim.tbl_deep_extend("force", opts, user_opts or {})
+  if opts.git_timeout then
+    git.git_timeout = opts.git_timeout
+  end
 end
 
 function M.get()
